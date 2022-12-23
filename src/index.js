57d120d857d590805320797d53714d6a2738f0d9
androkurniawan <androkurniawan7@gmail.com>
2022-12-23 23:58:52 +0700

    "Second commit"

--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import RegisterHotel from './components/RegisterHotel';
+import Login from './components/Login';
+import BodyIndex from './components/BodyIndex';
+import RegisterCustomer from './components/RegisterCustomer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+
+      <Routes>
+        
+        <Route path='/' element={<App />}>
+          <Route path='/' element={<BodyIndex />} />
+          <Route path='/register-hotel' element={<RegisterHotel />} />
+        </Route>
+
+        <Route path='/login' element={<Login />} />
+        <Route path='/register-customer' element={<RegisterCustomer />} />
+
+      </Routes>
+
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
